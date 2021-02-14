46de46ece2c6caa00adfdc107f264de9c9dd18df
Basil-Panasiuk <vasylpanasjuk2001@gmail.com>
2021-02-14 18:37:40 +0300

    add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  let ob = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+    '|': '|'
+  }
+  let st = [];
+  let b = [')',']','}','|'];
+  
+  for ( let i=0; i< arr.length; i++){
+
+    if( b.includes(arr[i]) ) {
+      if(st[st.length-1] == ob[arr[i]]) {
+        st.pop();
+      } else {
+        st.push(arr[i]);
+      }
+    } else {
+      st.push(arr[i]);
+    }
+  }
+
+  return st.length == 0;
+
 }
